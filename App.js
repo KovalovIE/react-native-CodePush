1643558c233a89bb6b7e2014cfd5df152e6bb6c6
KovalovIE <kovalevie1@gmail.com>
2020-01-06 13:38:58 +0200

    change behavior Codepush3

--- App.js
+++ App.js
@@ -1,129 +1,46 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow
-//  */
-
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   ScrollView,
-//   View,
-//   Text,
-//   StatusBar,
-//   TouchableOpacity,
-// } from 'react-native';
-// import codePush from "react-native-code-push";
-
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// const App: () => React$Node = () => {
+// class App extends Component<Props> {
   
-//   return (
-//     <>
-//       <StatusBar barStyle="dark-content" />
-//       <SafeAreaView>
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={styles.scrollView}>
-//           <Header />
-//           {global.HermesInternal == null ? null : (
-//             <View style={styles.engine}>
-//               <Text style={styles.footer}>Engine: Hermes</Text>
-//             </View>
-//           )}
-//           <View style={styles.body}>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Step One</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-//                 screen and then come back to see your edits.
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>See Your Changes</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <ReloadInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Debug</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <DebugInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Learn More</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Read the docs to discover what to do next:
-//               </Text>
-//             </View>
-//             <TouchableOpacity onPress={this.onButtonPress}>
-//               <Text>Check for updates</Text>
-//             </TouchableOpacity>
-//             {/* <LearnMoreLinks /> */}
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </>
-//   );
-// };
+//   onButtonPress() {
+//     // codePush.sync({
+//     //   updateDialog: true,
+//     //   installMode: codePush.InstallMode.IMMEDIATE
+//     // });
+//     alert('Hello')
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>Enjoy!!!</Text>
+//         <Text style={styles.welcome}>UHUHUHUH!!!</Text>
+//         {/* <TouchableOpacity onPress={this.onButtonPress}>
+//           <Text>Hello</Text>
+//         </TouchableOpacity> */}
+//       </View>
+//     );
+//   }
+// }
+
+// export default codePush({ checkFrequency: codePush.CheckFrequency.ON_APP_START })(App);
 
 // const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#F5FCFF"
 //   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: "center",
+//     margin: 10
 //   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
+//   instructions: {
+//     textAlign: "center",
+//     color: "#333333",
+//     marginBottom: 5
+//   }
 // });
-
-// export default App;
-
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from "react";
 import {
@@ -166,7 +83,9 @@ class App extends Component<Props> {
   }
 }
 
-export default codePush({ checkFrequency: codePush.CheckFrequency.ON_APP_START })(App);
+App = codePush(App);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
