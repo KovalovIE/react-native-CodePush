a30c1301ebbb2e2f8701be8a6de4b5775cc59e52
KovalovIE <kovalevie1@gmail.com>
2020-01-21 23:36:21 +0200

    add App.js

--- App.js
+++ App.js
@@ -1,55 +1,7 @@
-// class App extends Component<Props> {
-  
-//   onButtonPress() {
-//     // codePush.sync({
-//     //   updateDialog: true,
-//     //   installMode: codePush.InstallMode.IMMEDIATE
-//     // });
-//     alert('Hello')
-//   }
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>Enjoy!!!</Text>
-//         <Text style={styles.welcome}>UHUHUHUH!!!</Text>
-//         {/* <TouchableOpacity onPress={this.onButtonPress}>
-//           <Text>Hello</Text>
-//         </TouchableOpacity> */}
-//       </View>
-//     );
-//   }
-// }
-
-// export default codePush({ checkFrequency: codePush.CheckFrequency.ON_APP_START })(App);
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#F5FCFF"
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: "center",
-//     margin: 10
-//   },
-//   instructions: {
-//     textAlign: "center",
-//     color: "#333333",
-//     marginBottom: 5
-//   }
-// });
-
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import codePush from "react-native-code-push";
+import Buttons from "./app/components/Buttons/button";
 
 type Props = {};
 
@@ -76,11 +28,8 @@ class App extends Component<Props> {
     return (
       isShow
       ? <View style={styles.container}>
-          <Text style={styles.welcome}>Enjoy!!!</Text>
           <Text style={styles.welcome}>UPDATE DONE!!!</Text>
-          <TouchableOpacity onPress={this.onButtonPress}>
-            <Text>Click</Text>
-          </TouchableOpacity>
+          <Buttons btnRight='btnRight' btnLeft='btnLeft' />
         </View>
       : <Text style={styles.welcome}>SplashScreen</Text>
     );
